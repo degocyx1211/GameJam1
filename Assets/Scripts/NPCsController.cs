2d920371e1e0574e8d9ad7b404c81314fb7345c1
Hernán Leonardo Quiroga Beltrán <126023723+Leo-Quiroga@users.noreply.github.com>
2024-03-07 16:23:18 -0500

    Leonardo2 Player y NPC básicos (#3)
    * PlayerControllerInicio
    
    Inicio del player controller
    
    * Player Idle y Mov básico
    
    Player se mantine en Idle y el movimiento básico funciona.
    
    * PlayerControllerFuncional
    
    Prototipo camina y es funcional.
    
    * NPC colisiona con Player
    
    NPC Colisiona con el player y cambia estado de ánimo
    
    * TMP descargado
    
    TMP descargado
    
    * Cambio nombre scene

---
+++ Assets/Scripts/NPCsController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCsController : MonoBehaviour
+{
+    public bool happy;
+    public bool angry;
+
+    public Animator animatorNPC;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        angry = true;
+        happy = false;
+        animatorNPC = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Afecta el grounded para saber cuando está en el suelo
+        animatorNPC.SetBool("happy", happy);
+        animatorNPC.SetBool("angry", angry);
+    }
+
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            angry = false;
+            happy = true;
+        }
+    }
+
+
+
+
+}
