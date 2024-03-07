71ad48fb1b8068d4c533830a4a93a8877c33b177
Hernán Leonardo Quiroga Beltrán <hleonardoquirogab@hotmail.com>
2024-03-07 15:24:28 -0500

    PlayerControllerFuncional
    Prototipo camina y es funcional.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -37,58 +37,69 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float yStore = moveDirection.y;
+        MovePlayer();
+        JumpPlayer();
+        PlayerRotation();
 
-        //Movimiento
+        //Gravedad
+        moveDirection.y += Physics.gravity.y * Time.deltaTime * garavityScale;
+
+        SentAnimatorInfo();
+    }
+
+    //Movimiento
+    void MovePlayer()
+    {
+        float yStore = moveDirection.y;
         moveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"));
         moveDirection.Normalize();
         moveDirection = moveDirection * moveSpeed;
         moveDirection.y = yStore;
 
-
         charController.Move(moveDirection * Time.deltaTime);
+    }
 
-        //Salto
+    void JumpPlayer()
+    {
+        if (charController.isGrounded)
+        {
+            moveDirection.y = -1f;
 
-        //if (charController.isGrounded)
-        //{
-        //    moveDirection.y = -1f;
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpForce;
+                doubleJump = true;
+            }
+        }
+        else if (Input.GetButtonDown("Jump") && doubleJump == true)
+        {
+            moveDirection.y = jumpForce;
+            doubleJump = false;
+        }
+    }
 
-        //    if (Input.GetButtonDown("Jump"))
-        //    {
-        //        moveDirection.y = jumpForce;
-        //        doubleJump = true;
-        //    }
-        //}
-        //else if (Input.GetButtonDown("Jump") && doubleJump == true)
-        //{
-        //    moveDirection.y = jumpForce;
-        //    doubleJump = false;
-        //}
+    void PlayerRotation()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            //El player rota con la cámara
+            transform.rotation = Quaternion.Euler(0f, playerCamera.transform.rotation.eulerAngles.y, 0f);
 
+            //El player rotahacia la dirección a donde camina
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
 
-        //Gravedad
-        moveDirection.y += Physics.gravity.y * Time.deltaTime * garavityScale;
+            //Rota suavemente
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+        }
+    }
 
-
-        //Solo rota si hay movimiento del Player
-        //if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
-        //{
-        //    //El player rota con la cámara
-        //    transform.rotation = Quaternion.Euler(0f, playerCamera.transform.rotation.eulerAngles.y, 0f);
-
-        //    //El player rotahacia la dirección a donde camina
-        //    Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
-
-        //    //Rota suavemente
-        //    playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
-
-        //}
-
-        ////afecta los datos del animator. Le envía datos al parametro Speed
-        //animator.SetFloat("Speed", Mathf.Abs(moveDirection.x) + Mathf.Abs(moveDirection.z));
+    void SentAnimatorInfo()
+    {
+        //afecta los datos del animator. Le envía datos al parametro Speed
+        animator.SetFloat("Speed", Mathf.Abs(moveDirection.x) + Mathf.Abs(moveDirection.z));
         ////Afecta el grounded para saber cuando está en el suelo
         //animator.SetBool("Grounded", charController.isGrounded);
-
     }
+
+
 }
